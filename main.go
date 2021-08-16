c1dbca331f6187bcd71f8208eec0f794067508b0
nugroho <nugroho@mackbookpro.local>
2021-08-16 15:05:49 +0700

    Initial

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ func parent() {
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
+
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
